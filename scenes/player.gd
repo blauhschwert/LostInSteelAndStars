c92f28c5fc34406444d893ca85ca4dd3746c7cdf
Blauhschwert <komeil13@yahoo.de>
2026-04-04 20:57:59 +0200

    a little bit more friends to have a better world

---
+++ scenes/player.gd
@@ -0,0 +1,80 @@
+class_name Player
+extends CharacterBody3D
+
+var move_distance : float = 3.0
+var move_speed : float = 0.5
+var turn_speed : float = 0.2
+var target_position : Vector3
+var target_rotation : float
+var is_moving: bool
+
+@onready var forward_ray: RayCast3D = $ForwardRay
+@onready var backward_ray: RayCast3D = $BackwardRay
+@onready var left_ray: RayCast3D = $LeftRay
+@onready var right_ray: RayCast3D = $RightRay
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	target_position = global_position
+
+func _input(event: InputEvent) -> void:
+	if not is_moving:
+		if Input.is_action_just_pressed("move_forward"):
+			move_forward()
+		elif Input.is_action_just_pressed("move_backward"):
+			move_backward()
+		elif Input.is_action_just_pressed("move_left"):
+			move_left()
+		elif Input.is_action_just_pressed("move_right"):
+			move_right()
+		elif Input.is_action_just_pressed("turn_left"):
+			turn_left()
+		elif Input.is_action_just_pressed("turn_right"):
+			turn_right()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	if is_moving:
+		if not global_position == target_position:
+			global_position = lerp(global_position, target_position, move_speed)
+			if global_position.distance_to(target_position) < 0.1:
+				global_position = target_position
+				is_moving = false
+
+func move_forward():
+	if not forward_ray.is_colliding():
+		target_position -= global_transform.basis.z * move_distance
+		is_moving = true
+
+func move_backward():
+	if not backward_ray.is_colliding():
+		target_position += global_transform.basis.z * move_distance
+		is_moving = true
+
+func move_left():
+	if not left_ray.is_colliding():
+		target_position += global_transform.basis.x * move_distance
+		is_moving = true
+
+func move_right():
+	if not right_ray.is_colliding():
+		target_position -= global_transform.basis.x * move_distance
+		is_moving = true
+
+func turn_left():
+	is_moving = true
+	target_rotation = rotation.y + PI/2.0
+	var tween = create_tween()
+	tween.tween_property(self, "rotation:y", target_rotation,turn_speed)
+	tween.finished.connect(func(): is_moving = false)
+
+
+func turn_right():
+	is_moving = true
+	target_rotation = rotation.y - PI/2.0
+	var tween = create_tween()
+	tween.tween_property(self, "rotation:y", target_rotation,turn_speed)
+	tween.finished.connect(func(): is_moving = false)
